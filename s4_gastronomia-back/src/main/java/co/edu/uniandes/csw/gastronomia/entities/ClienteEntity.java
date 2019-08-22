fc1f132c2d04857e4b478d87d067d8fb84f58ea2
Cristina González <ci.gonzalez10@uniandes.edu.co>
2019-08-22 16:43:03 -0500

    Persistencia de las clases de Cliente y Reserva

---
+++ s4_gastronomia-back/src/main/java/co/edu/uniandes/csw/gastronomia/entities/ClienteEntity.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.gastronomia.entities;
+
+import java.util.Date;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Cristina González
+ */
+@Entity
+public class ClienteEntity extends BaseEntity{
+    
+    private Date cumpleanos;
+    
+    private String numeroContacto;
+    
+    private int puntos;
+
+    public ClienteEntity() {
+    }
+
+    public ClienteEntity(Date cumpleanos, String numeroContacto, int puntos) {
+        this.cumpleanos = cumpleanos;
+        this.numeroContacto = numeroContacto;
+        this.puntos = puntos;
+    }
+
+    /**
+     * @return the cumpleanos
+     */
+    public Date getCumpleanos() {
+        return cumpleanos;
+    }
+
+    /**
+     * @return the numeroContacto
+     */
+    public String getNumeroContacto() {
+        return numeroContacto;
+    }
+
+    /**
+     * @return the puntos
+     */
+    public int getPuntos() {
+        return puntos;
+    }
+
+    /**
+     * @param cumpleanos the cumpleanos to set
+     */
+    public void setCumpleanos(Date cumpleanos) {
+        this.cumpleanos = cumpleanos;
+    }
+
+    /**
+     * @param numeroContacto the numeroContacto to set
+     */
+    public void setNumeroContacto(String numeroContacto) {
+        this.numeroContacto = numeroContacto;
+    }
+
+    /**
+     * @param puntos the puntos to set
+     */
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }   
+    
+}
