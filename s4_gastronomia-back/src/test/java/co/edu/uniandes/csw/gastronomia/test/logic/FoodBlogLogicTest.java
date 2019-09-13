325a2980ba93c41097464ab9567a31ded718c280
David <d.martinezg2uniandes.edu.co>
2019-09-13 18:03:10 -0500

    Cambios en la capa logica de las clases foodblog y de la clase resena

--- s4_gastronomia-back/src/test/java/co/edu/uniandes/csw/gastronomia/test/logic/FoodBlogLogicTest.java
+++ s4_gastronomia-back/src/test/java/co/edu/uniandes/csw/gastronomia/test/logic/FoodBlogLogicTest.java
@@ -5,14 +5,99 @@
  */
 package co.edu.uniandes.csw.gastronomia.test.logic;
 
+import co.edu.uniandes.csw.gastronomia.entities.FoodBlogEntity;
+import co.edu.uniandes.csw.gastronomia.persistence.FoodBlogPersistence;
+import co.edu.uniandes.csw.gastronomia.ejb.FoodBlogLogic;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
- * @author David Martinez
+ *  food David Martinez
  */
 @RunWith(Arquillian.class)
 public class FoodBlogLogicTest {
+    private PodamFactory factory = new PodamFactoryImpl();
     
+    @Inject
+    private FoodBlogLogicTest foodblogLogic;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    private UserTransaction utx;
+    
+     private List<FoodBlogEntity> data = new ArrayList<>();
+     
+     /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(FoodBlogEntity.class.getPackage())
+                .addPackage(FoodBlogLogic.class.getPackage())
+                .addPackage(FoodBlogPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+      /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from FoodBlogEntity").executeUpdate();
+        
+    }
+    
+     /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            FoodBlogEntity entity = factory.manufacturePojo(FoodBlogEntity.class);
+            em.persist(entity);
+                        data.add(entity);
+        }
+        FoodBlogEntity foodb = data.get(2);
+    }
 }
+    
+   
