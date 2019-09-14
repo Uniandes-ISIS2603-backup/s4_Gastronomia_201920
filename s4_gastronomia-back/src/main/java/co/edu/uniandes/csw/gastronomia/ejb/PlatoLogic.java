aae426a55caaa92c417070b04b29457e471dcaa5
jecanizarez <je.canizarez@uniandes.edu.co>
2019-09-14 16:55:23 -0500

    Terminacion capa logica con sus respectivas pruebas unitarias

--- s4_gastronomia-back/src/main/java/co/edu/uniandes/csw/gastronomia/ejb/PlatoLogic.java
+++ s4_gastronomia-back/src/main/java/co/edu/uniandes/csw/gastronomia/ejb/PlatoLogic.java
@@ -8,6 +8,7 @@ package co.edu.uniandes.csw.gastronomia.ejb;
 import co.edu.uniandes.csw.gastronomia.entities.PlatoEntity;
 import co.edu.uniandes.csw.gastronomia.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.gastronomia.persistence.PlatoPersistence;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 /**
@@ -43,7 +44,58 @@ public class PlatoLogic {
             throw new BusinessLogicException("El plato ya existe");
         }
         
-        plato = persistence.create(plato);
-        return plato; 
+        return persistence.create(plato);
     }
+    /**
+     * Metodo para actualizar un plato 
+     * @param platoId. Id del plato que se quiere actualizar
+     * @param plato. Plato con la informacion a actualizar
+     * @return plato con la informacion actualizada
+     * @throws BusinessLogicException 
+     */
+    public PlatoEntity updatePlato(Long platoId, PlatoEntity plato  )throws BusinessLogicException
+    {
+         if(plato.getPrecio() < 0)
+        {
+            throw new BusinessLogicException("El precio del plato es  negativo");
+        }
+        else if(plato.getNombreComida() == null)
+        {
+            throw new BusinessLogicException("El plato no tiene un nombre");
+        }
+        else if(plato.getDescripcion() == null)
+        {
+            throw new BusinessLogicException("El plato no tiene una descripcion");
+        }
+         return persistence.update(plato);
+         
+    }
+    /**
+     * Metodo para eliminar un plato segun el id especificado
+     * @param tarjetaId 
+     */
+    public void deletePlato(Long platoId)
+    {
+        persistence.delete(platoId);
+    }
+    
+    /**
+     * Metodo para encontrar un plato segun el id indicado
+     * @param platoId. Id del plato que se quiere encontrar
+     * @return Plato
+     */
+    public PlatoEntity findPlato(Long platoId)
+    {
+        return persistence.find(platoId); 
+    }
+    /**
+     * Metodo para encontrar todos los platos.
+     * @return 
+     */
+    public List<PlatoEntity> findAllPlato()
+    {
+        return persistence.findAll(); 
+    }
+    
+    
 }
