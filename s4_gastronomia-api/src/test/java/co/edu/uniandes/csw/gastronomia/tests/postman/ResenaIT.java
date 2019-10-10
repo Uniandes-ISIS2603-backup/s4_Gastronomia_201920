a29d6dd6728e46ca113cc0369056364ddcdba77d
David <d.martinezg2uniandes.edu.co>
2019-10-10 16:46:36 -0500

    Cambios 2

---
+++ s4_gastronomia-api/src/test/java/co/edu/uniandes/csw/gastronomia/tests/postman/ResenaIT.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.gastronomia.tests.postman;
+import co.edu.uniandes.csw.gastronomia.dtos.AdministradorDTO;
+import co.edu.uniandes.csw.gastronomia.dtos.CascaraDTO;
+import co.edu.uniandes.csw.gastronomia.mappers.BusinessLogicExceptionMapper;
+import co.edu.uniandes.csw.gastronomia.resources.AdministradorResource;
+import co.edu.uniandes.csw.gastronomia.resources.RestConfig;
+import co.edu.uniandes.csw.postman.tests.PostmanTestBuilder;
+import java.io.File;
+import java.io.IOException;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Estudiante
+ */
+@RunWith(Arquillian.class)
+public class ResenaIT {
+    
+    private static final String COLLECTION = "ResenaResourceTest.postman_collection";
+
+    @Deployment(testable = true)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "s4_gastronomia-api.war")//War del modulo api
+                // Se agrega las dependencias
+                .addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml")
+                        .importRuntimeDependencies().resolve()
+                        .withTransitivity().asFile())
+                // Se agregan los compilados de los paquetes de servicios
+                .addPackage(RestConfig.class.getPackage()) //No importa cual recurso usar, lo importante es agregar el paquet
+                .addPackage(CascaraDTO.class.getPackage()) //No importa cual dto usar, lo importante es agregar el paquete.
+                .addPackage(BusinessLogicExceptionMapper.class.getPackage())
+                // El archivo que contiene la configuracion a la base de datos.
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                // El archivo beans.xml es necesario para injeccion de dependencias.
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"))
+                // El archivo web.xml es necesario para el despliegue de los servlets
+                .setWebXML(new File("src/main/webapp/WEB-INF/web.xml"))
+                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/glassfish-resources.xml"));
+    }
+
+    @Test
+    @RunAsClient
+    public void postman() throws IOException {
+        PostmanTestBuilder tp = new PostmanTestBuilder();
+        tp.setTestWithoutLogin(COLLECTION, "Entorno-Colecciones.postman_environment");
+        String desiredResult1 = "0";
+        String desiredResult2 = "10";
+        String desiredResult3 = "9";
+        String desiredResult4 = "0";
+        String desiredResult5 = "13";
+        
+        //int desiredResult = 0;
+        System.out.println(tp.getIterations_failed());
+       Assert.assertEquals("Error en Iterations de: " + COLLECTION, desiredResult1, tp.getIterations_failed());
+
+        Assert.assertEquals("Error en Requests de: " + COLLECTION, desiredResult2, tp.getRequests_failed());
+
+        Assert.assertEquals("Error en Test-Scripts de: " + COLLECTION, desiredResult3, tp.getTest_scripts_failed());
+
+        Assert.assertEquals("Error en Assertions de: " + COLLECTION, desiredResult5, tp.getAssertions_failed());
+    }
+    
+}
