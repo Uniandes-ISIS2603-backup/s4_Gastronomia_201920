cc45632cb3a666634494c0b1d3ce604c9d4249f2
ci.gonzalez10 <ci.gonzalez10@uniandes.edu.co>
2019-12-04 18:15:48 -0500

    Solve merge

--- s4_gastronomia-api/src/main/java/co/edu/uniandes/csw/gastronomia/dtos/RestauranteDetailDTO.java
+++ s4_gastronomia-api/src/main/java/co/edu/uniandes/csw/gastronomia/dtos/RestauranteDetailDTO.java
@@ -42,6 +42,7 @@ public class RestauranteDetailDTO extends RestauranteDTO implements Serializable
         super();
     }
     
+    @Override
     public RestauranteEntity toEntity()
     {
         RestauranteEntity r = super.toEntity();
